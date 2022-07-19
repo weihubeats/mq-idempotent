abedf6284624247bfa2ac3317ab75cff14f9b494
weihu <weihubeats@163.com>
2022-07-19 19:17:47 +0800

    完善飞书通知

--- wh-core/src/test/java/com/mq/idempotent/core/alert/strategy/AlertStrategyFactoryTest.java
+++ wh-core/src/test/java/com/mq/idempotent/core/alert/strategy/AlertStrategyFactoryTest.java
@@ -1,6 +1,5 @@
 package com.mq.idempotent.core.alert.strategy;
 
-import com.mq.idempotent.core.spi.ExtensionLoader;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,8 +13,7 @@ public class AlertStrategyFactoryTest {
 
     @Test
     public void test() {
-        AlertStrategy lark = ExtensionLoader.getExtensionLoader(AlertStrategy.class).getJoin("lark");
-//        AlertStrategy lark = AlertStrategyFactory.newInstance("lark");
+        AlertStrategy lark = AlertStrategyFactory.newInstance("lark");
         assertTrue(lark instanceof LarkAlarmStrategy);
     }
     
